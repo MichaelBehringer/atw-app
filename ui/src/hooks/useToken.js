2ff2a5698f02db7b272867227c41f6633b2c8e13
Michael Behringer <michabehringer@gmail.com>
2023-12-27 21:54:28 +0100

    Save Token in local Storage

--- ui/src/hooks/useToken.js
+++ ui/src/hooks/useToken.js
@@ -4,19 +4,19 @@ import {useState} from 'react';
 function useToken() {
 
   function getToken() {
-    const userToken = sessionStorage.getItem('token');
+    const userToken = localStorage.getItem('token');
     return userToken && userToken;
   }
 
   const [token, setToken] = useState(getToken());
 
   function saveToken(userToken) {
-    sessionStorage.setItem('token', userToken);
+    localStorage.setItem('token', userToken);
     setToken(userToken);
   };
 
   function removeToken() {
-    sessionStorage.removeItem("token");
+    localStorage.removeItem("token");
     setToken(null);
   }
   return {
